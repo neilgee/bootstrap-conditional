4d622d9e208537b2c32032e7d7b0ea493da140cb
Neil Gee <neil@wpbeaches.com>
2021-03-09 10:48:21 +1100

    update to 1.3.1

--- bootstrap_conditional.php
+++ bootstrap_conditional.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://wordpress.org/plugins/bootstrap-conditional/
  * Description: Bootstrap Conditional - load full Bootstrap version 3,4 or 5
  * Author: <a href="https://wpbeaches.com">Neil Gowran</a>,
- * Version: 1.3.0
+ * Version: 1.3.1
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: bootstrap-conditional
